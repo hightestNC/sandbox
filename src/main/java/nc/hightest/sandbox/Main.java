95ba0b401541928e21235f02d88ba3b0403c81d3
matth <matthieu@hightest.nc>
2017-05-04 11:19:42 +1100

    coco lasticot

--- src/main/java/nc/hightest/sandbox/Main.java
+++ src/main/java/nc/hightest/sandbox/Main.java
@@ -1,6 +1,8 @@
+package nc.hightest.sandbox;
 /**
  * Created by matth on 03/05/2017.
  */
+
 public class Main {
     public static void main (String [] args) {
         System.out.println("coucou");
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,3 +1,4 @@
+import nc.hightest.sandbox.Library;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
